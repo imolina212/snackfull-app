118253b5f19626aa64c93059f60e988c77a8505b
salrey <tekdehn@gmail.com>
2022-02-11 23:27:16 -0500

    Update isValid

--- back-end/helpers/functions.js
+++ back-end/helpers/functions.js
@@ -6,7 +6,7 @@ const setValues = (snack) => {
     if (!snack.image) {
         snack.image = "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image"
     }
-    
+
     if (snack.name.length > 2) {
         const list = snack.name.split(" ");
         snack.name = list.forEach((word) => {
@@ -24,10 +24,10 @@ const setValues = (snack) => {
 const isValid = (snack) => {
     if (
       typeof snack.name === "string" &&
-      typeof snack.image === "string" &&
-      !isNaN(Number(snack.fiber)) &&
-      !isNaN(Number(snack.protein)) &&
-      !isNaN(Number(snack.added_sugar))
+      typeof snack.image === "string" 
+    //   !isNaN(Number(snack.fiber)) &&
+    //   !isNaN(Number(snack.protein)) &&
+    //   !isNaN(Number(snack.added_sugar))
     ) {
       return true;
     }
